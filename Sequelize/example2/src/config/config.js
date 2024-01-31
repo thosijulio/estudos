44d5d6dfee13f6e63bf596392ba232c9838a9f15
thosijulio <thosijulio@gmail.com>
2024-01-31 17:45:39 -0300

    compose - conectando sequelize da api ao banco

--- Sequelize/example2/src/config/config.js
+++ Sequelize/example2/src/config/config.js
@@ -2,8 +2,8 @@ const config = {
   username: 'root',
   password: '1312',
   database: 'orm_example',
-  host: 'localhost',
-  port: '3307',
+  host: 'database',
+  port: '3306',
   dialect: 'mysql',
 }
 
